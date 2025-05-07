b81e1ba8792d9e72e58a736a06d4075f4060c779
O12097 <desyaderina@gmail.com>
2025-05-07 11:38:20 +0800

    style: penyesuaian ServiceCard component styles for improved layout and typography

--- src/components/detail/ServiceCard.jsx
+++ src/components/detail/ServiceCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ServiceCard = ({ title, description }) => {
   return (
-    <article className="flex flex-col items-center p-7 w-full bg-orange-50 rounded-lg shadow-sm h-[260px]">
+    <article className="flex flex-col items-center p-7 w-full bg-orange-50 rounded-lg shadow-sm h-auto">
       <h3 className="mb-6 text-3xl text-center font-bold text-stone-950">
         {title}
       </h3>
-      <p className="text-base font-light text-center text-stone-950">
+      <p className="text-2xl font-medium text-center text-stone-950">
         {description}
       </p>
     </article>
